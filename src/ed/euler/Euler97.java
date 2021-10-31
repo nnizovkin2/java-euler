889987f59904f40823b876493a50967abbd693a5
n <nnizovkin>
2021-10-31 20:49:56 +0300

    Девяносто седьмая задача.

---
+++ src/ed/euler/Euler97.java
@@ -0,0 +1,9 @@
+package ed.euler;
+
+import java.math.BigInteger;
+
+public class Euler97 {
+    public static void main(String[] args) {
+        System.out.println(BigInteger.TWO.pow(7830457).multiply(new BigInteger("28433")).add(BigInteger.ONE));
+    }
+}
