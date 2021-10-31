65d14f1d9c6a8d3afa5c994515a3abcb0d9d2717
n <nnizovkin>
2021-10-31 20:51:00 +0300

    Добавил вывод времени работы для шестидесятой задачи.

--- src/ed/euler/Euler60.java
+++ src/ed/euler/Euler60.java
@@ -3,10 +3,12 @@ import ed.euler.graph.*;
 
 public class Euler60 {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         Graph g = PrimeGraphFactory.buildGraph(10000);
         var ss = new ExactCliqueFinder(g);
         var c = ss.findClique(5).stream().mapToInt(i->i).sum();
         System.out.println(c);
+        System.out.println(System.currentTimeMillis()  - start);
     }
 
 }
