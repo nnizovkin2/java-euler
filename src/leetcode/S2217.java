58d42548a5cf5a219fcb19c9c39dacad07ccdbd1
n <nnizovkin>
2022-03-30 07:56:38 +0300

    apdate S2217

--- src/leetcode/S2217.java
+++ src/leetcode/S2217.java
@@ -10,43 +10,71 @@ public class S2217 {
     }
 
     private long gen(int query, int l) {
-        if(l==1) {
-            if(query<10) {
+        if (l == 1) {
+            if (query < 10) {
                 return query;
             } else {
                 return -1;
             }
         }
 
-        if (9 * (Math.pow(10, (l & 1) + (l >> 1) - 1)) < query) {
+        if (9 * (pow(10, (l & 1) + (l >> 1) - 1)) < query) {
             return -1;
         }
 
         query--;
-        long pref = 0;
         long suffix = 0;
         int dn = (l & 1) + l >> 1;
         int itN = 0;
         long m = 1;
+        long middle = 0;
         while (query != 0) {
             int d = query % 10;
             if (itN == 0 && (l & 1) == 1) {
-                pref = d;
-            } else if((l & 1) == 1) {
-                m *= 100;
-                pref = m * d + pref*10;
-                suffix = suffix * 10 + d;
+                middle = d;
             } else {
-                pref = 10 * (m * d + pref);
                 suffix = suffix * 10 + d;
-                m*=100;
             }
             query /= 10;
             itN++;
         }
-        pref = (long)(Math.pow(10, l - 1) + pref * Math.pow(10, dn-itN));
-        suffix = 1 + suffix * (long) Math.pow(10, dn - itN);
+        suffix = 1 + suffix * pow(10, dn - itN);
+        long pref = 0;
+        long ts = suffix;
+        int hs = l >> 1;
+        while (hs != 0) {
+            pref = pref * 10 + ts % 10;
+            ts /= 10;
+            hs--;
+        }
+        if ((l & 1) == 1) {
+            pref = pref * 10 + middle;
+        }
+
+        pref *= pow(10, l >> 1);
 
         return pref + suffix;
+    }
+
+    long pow(int x, int y) {
+        long res = 1;
+        int y2 = revert(y);
+        while (y != 0) {
+            res = res * res * ((y2 & 1) == 1 ? x : 1);
+            y2 >>= 1;
+            y >>= 1;
+        }
+        return res;
+    }
+
+    int revert(int l) {
+        int r = 0;
+        while (l != 0) {
+            r <<= 1;
+            r += l & 1;
+            l >>= 1;
+        }
+
+        return r;
     }
 }
